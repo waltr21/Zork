9be74f78e2c0336c49397315c733dfa5068515c0
Ryan Walt <waltr@mail.gvsu.edu>
2017-11-20 11:36:27 -0500

    Delete Home.py

--- Home.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-class Home:
-    def __init__(self):
-        tempNum = randint(0,10)
-        self.homeList = [None] * tempNum
-
-    def getMonsters(self):
-        return homeList
